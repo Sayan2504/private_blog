2f4a40d538268693b8327be83df562f039a1ca72
Sayan2504 <sinhasayan888@gmail.com>
2026-08-15 12:55:10 +0530

    Ensure 16-byte key derivation for Active Record encryption

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  # If manually constructing a 16-byte key for AES-128:
+  raw_secret = ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"]
+  key = Digest::SHA256.digest(raw_secret.to_s)[0..15] if raw_secret.present?
 end
